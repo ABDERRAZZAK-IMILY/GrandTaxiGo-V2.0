481f6db80db2d9e1e6ee382d4d57a78cf2dcd49f
abderrazzak imily <imily2024@gmail.com>
2025-02-24 11:00:21 +0000

    create database megrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('role', ['passenger', 'driver'])->default('passenger');
+            $table->string('profile_picture')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
