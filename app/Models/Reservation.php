be11ae00d6d651606645bd97cae4df29b79720fa
abderrazzak imily <imily2024@gmail.com>
2025-02-24 11:26:09 +0000

    fix reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Reservation extends Model
 {
-    //
+    
+
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'trip_id',
+        'status'
+    ];
 
     public function user() {
         return $this->belongsTo(User::class);
