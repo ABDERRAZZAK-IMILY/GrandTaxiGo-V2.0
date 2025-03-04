9fd7b63c29de3a019e453196f1eefb1beaea0c1f
abderrazzak imily <imily2024@gmail.com>
2025-03-04 10:46:06 +0000

    add rating model

---
+++ app/Models/Rating.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Rating extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 
+        'rated_by',
+        'trip_id',  
+        'rating',  
+        'comment',  
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function ratedBy()
+    {
+        return $this->belongsTo(User::class, 'rated_by');
+    }
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+}
